bd7a1c478fd6328a6c1e61c3cfdf86aeca07efc6
patriciopsp <patricioandrespsp@gmail.com>
2024-07-12 01:57:30 -0400

    smooth scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,19 @@ $('#enviarcorreo').on('click', function() {
 });
 
 
-
+$(document).ready(function(){
+    $("#").on('click', function(event) {
+    if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+    scrollTop: $(hash).offset().top
+    }, 10, function(){
+   
+    window.location.hash = hash;
+    });
+    }
+    });
+   });
+   
+   
